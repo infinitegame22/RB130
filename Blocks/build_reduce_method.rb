ad905fa036523db8352369d6c172c17e799f293b
Amy Dripchak <adripchak@yahoo.com>
2023-05-04 08:02:46 -0600

    update

--- Blocks/build_reduce_method.rb
+++ Blocks/build_reduce_method.rb
@@ -43,4 +43,6 @@ def reduce(array, default=0)
 end
 # p reduce(array) {|acc, num| acc + num}
 # p reduce(array, 10) { |acc, num| acc + num }
-p reduce(array) { |acc, num| acc + num if num.odd? }
+# p reduce(array) { |acc, num| acc + num if num.odd? }
+p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
+p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
